f6b493ce0e0ed9e2bd28b7baa7eb8dae6f7017b8
xljnc <jsj0804wt@126.com>
2018-12-07 18:42:56 +0800

    first commit

---
+++ src/main/java/com/wt/test/sharding/sphere/ShardingSphereServerParentApplication.java
@@ -0,0 +1,12 @@
+package com.wt.test.sharding.sphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingSphereServerParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingSphereServerParentApplication.class, args);
+    }
+}
