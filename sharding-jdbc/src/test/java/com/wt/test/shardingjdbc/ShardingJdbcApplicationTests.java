a8233e87008646c3798fdf59030f2df5c954ec0b
qiyu <jsj0804wt@126.com>
2019-01-19 21:13:06 +0800

    添加sharding-jdbc模块 by 埼玉 2019-Jan-19

---
+++ sharding-jdbc/src/test/java/com/wt/test/shardingjdbc/ShardingJdbcApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wt.test.shardingjdbc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShardingJdbcApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
