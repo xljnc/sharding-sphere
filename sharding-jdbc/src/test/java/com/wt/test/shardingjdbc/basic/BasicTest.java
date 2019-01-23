7dda636e793da848ae5e528426bf0cb8fbbb4d38
qiyu <jsj0804wt@126.com>
2019-01-23 21:51:52 +0800

    添加book模块 by 埼玉 2019-Jan-23

---
+++ sharding-jdbc/src/test/java/com/wt/test/shardingjdbc/basic/BasicTest.java
@@ -0,0 +1,16 @@
+package com.wt.test.shardingjdbc.basic;
+
+import org.junit.Test;
+
+/**
+ * @Auther: 埼玉
+ * @Date: 2019/1/23 17:45
+ * @Description:
+ */
+public class BasicTest {
+
+    @Test
+    public void testChar() {
+        System.out.println((int) 'a' % 10);
+    }
+}
