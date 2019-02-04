8fb629dbd67d668c76ca03e24afd62e2769571ff
xljnc <jsj0804wt@126.com>
2019-02-04 17:01:41 +0800

    添加配置文件 by Xljnc 2019-Feb-03

--- sharding-jdbc/src/main/java/com/wt/test/shardingjdbc/ShardingJdbcApplication.java
+++ sharding-jdbc/src/main/java/com/wt/test/shardingjdbc/ShardingJdbcApplication.java
@@ -2,8 +2,9 @@ package com.wt.test.shardingjdbc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ShardingJdbcApplication {
 
     public static void main(String[] args) {
